31eb855d351ef779cd780fe3ed905c4987afe01e
JingZhouLiuDeHua <yyyttt2016@outlook.com>
2019-07-27 17:27:51 -0700

    s
    s

---
+++ leetcode_JAVA/Java/mid/UseJava8Lambda/topKFrequentElements_347.java
@@ -0,0 +1,31 @@
+package leetcode.mid.UseJava8Lambda;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class topKFrequentElements_347 {
+	public List<Integer> topKFrequent(int[] nums, int k) {
+	    HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+ 
+        List<Integer> lamdaSort = new ArrayList<Integer>();
+        map.entrySet().stream()
+                            .sorted((p1, p2) -> p1.getValue() != p2.getValue() ? 
+                                                p2.getValue() - p1.getValue() : p1.getKey().compareTo(p2.getKey()))
+                            .limit(k)
+                            .forEachOrdered(x-> lamdaSort.add(x.getKey()));
+
+        System.out.println(map);     
+        return lamdaSort;
+   }
+	
+	public static void main(String[] args) {
+	    int[] arr= {1,1,1,2,2,3};
+	    topKFrequentElements_347 test=new topKFrequentElements_347();
+	    List<Integer> res=test.topKFrequent(arr,2);
+	    System.out.println(res);
+	}
+}
---
+++ leetcode_JAVA/Java/mid/UseJava8Lambda/topKFrequentWords_692.java
@@ -0,0 +1,46 @@
+package leetcode.mid.UseJava8Lambda;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class topKFrequentWords_692 {
+
+	class Pair {
+        String word;
+        int freq;
+
+        Pair(String word, int freq) {
+            this.word = word;
+            this.freq = freq;
+        }
+    }
+
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        
+        List<String> lamdaSort = new ArrayList<String>();
+        map.entrySet().stream()
+                            .sorted((p1, p2) -> p1.getValue() != p2.getValue() ? 
+                                                p2.getValue() - p1.getValue() : p1.getKey().compareTo(p2.getKey()))
+                            .limit(k)
+                            .forEachOrdered(x-> lamdaSort.add(x.getKey()));
+
+        System.out.println(map);     
+        return lamdaSort;
+}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String[] arr= {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
+        topKFrequentWords_692 test=new topKFrequentWords_692();
+        List<String> res=test.topKFrequent(arr,2);
+        System.out.println(res);
+	}
+
+}
